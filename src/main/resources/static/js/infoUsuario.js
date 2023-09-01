f3ee5740b4320445bb9033299b4dd84dfae803c0
BryanAlexisMartinez <b.alexis.mtz.a@gmail.com>
2023-09-01 00:40:30 -0600

    Funcionalidad de login completa, prueba en infoUsuario

--- src/main/resources/static/js/infoUsuario.js
+++ src/main/resources/static/js/infoUsuario.js
@@ -1,137 +1,50 @@
 let tablainfo = document.getElementById("tablainfo");
 
-window.addEventListener("load", function (event) {
-    event.preventDefault();
-    tablainfo.insertAdjacentHTML("beforeend", `  
-        <tbody>
-        <!-- ================= Nombre usuario ============== -->
-        <tr>
-            <td>
-                <div>
-                    <label for="nombre" class="form-label" style="color:black;">
-                        <h3>Nombre:</h3>
-                    </label>
-                </div>
-            </td>
-            <th style="text-align: right;">
-                <div>
-                    <h3>${nombre}<a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#3a8761" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                    </svg></a></h3>
-                </div>
-            </th>
-        </tr>
-        <!-- ================= Apellido usuario ============== -->
-        <tr>
-            <td>
-                <div>
-                    <label for="apellido" class="form-label" style="color:black;">
-                        <h3>Apellido:</h3>
-                    </label>
-                </div>
-            </td>
-            <th style="text-align: right;">
-                <div>
-                    <h3>${apellido}<a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#3a8761" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                    </svg></a></h3>
-                </div>
-            </th>
-        </tr>
-        <!-- ================= Telefono usuario ============== -->
-        <tr>
-            <td>
-                <div>
-                    <label for="telefono" class="form-label" style="color:black;">
-                        <h3>Teléfono:</h3>
-                    </label>
-                </div>
-            </td>
-            <th style="text-align: right;">
-                <div>
-                    <h3>${telefono}<a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#3a8761" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                    </svg></a></h3>
-                </div>
-            </th>
-        </tr>
-        <!-- ================= Email usuario ============== -->
-        <tr>
-            <td>
-                <div>
-                    <label for="email" class="form-label" style="color:black;">
-                        <h3>Correo electrónico:</h3>
-                    </label>
-                </div>
-            </td>
-            <th style="text-align: right;">
-                <div>
-                    <h3>${email}<a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#3a8761" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                    </svg></a></h3>
-                </div>
-            </th>
-        </tr>
-        <!-- ================= Password usuario ============== -->
-        <tr>
-            <td>
-                <div>
-                    <label for="password" class="form-label" style="color:black;">
-                        <h3>Contraseña:</h3>
-                    </label>
-                </div>
-            </td>
-            <th style="text-align: right;">
-                <div>
-                    <h3>Actualizar contraseña <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#3a8761" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                    </svg></a></h3>
-                </div>
-            </th>
-        </tr>
-        <!-- ================= Domicilio usuario ============== -->
-        <tr>
-            <td>
-                <div>
-                    <label for="password" class="form-label" style="color:black;">
-                        <h3>Domicilio:</h3>
-                    </label>
-                </div>
-            </td>
-            <th style="text-align: right;">
-                <div>
-                    <h3>${domicilio}<a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#3a8761" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                    </svg></a></h3>
-                </div>
-            </th>
-        </tr>
-        <!-- ================= Estado usuario ============== -->
-        <tr>
-            <td>
-                <div>
-                    <label for="password" class="form-label" style="color:black;">
-                        <h3>Estado:</h3>
-                    </label>
-                </div>
-            </td>
-            <th style="text-align: right;">
-                <div>
-                    <h3>${estado}<a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#3a8761" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                    </svg></a></h3>
-                </div>
-            </th>
-        </tr>
+$(document).ready(function () {
+    if (sessionStorage.getItem('estadoLogin') === 'true') {
+        $.ajax({
+            url: 'https://kotomitl.onrender.com/api/usuarios/',
+            method: 'GET',
+            success: function (data) {
+                // Supongamos que el controlador devuelve un objeto JSON con los datos del usuario
+                let usuario = data;
 
-    </tbody>
-    `
-    );
-});// window load{}
+                // Limpia la tabla antes de agregar datos
+                tablainfo.innerHTML = '';
+
+                // Obtén los valores del usuario del objeto 'usuario'
+                let nombre = usuario.nombre;
+                let apellido = usuario.apellido;
+                let telefono = usuario.telefono;
+                let email = usuario.email;
+                let domicilio = usuario.domicilio;
+                let estado = usuario.estado;
+
+                // Inserta los valores en la tabla
+                tablainfo.insertAdjacentHTML("beforeend", `
+                    <tbody>
+                        <!-- ================= Nombre usuario ============== -->
+                        <tr>
+                            <td>
+                                <div>
+                                    <label for="nombre" class="form-label" style="color:black;">
+                                        <h3>Nombre:</h3>
+                                    </label>
+                                </div>
+                            </td>
+                            <th style="text-align: right;">
+                                <div>
+                                    <h3>${nombre}<a href="#"></a></h3>
+                                </div>
+                            </th>
+                        </tr>
+                        <!-- Resto de las filas con la misma estructura -->
+                    </tbody>
+                `);
+            },
+            error: function (error) {
+                console.error('Error al obtener los datos del usuario: ', error);
+            }
+        });
+    }
+});
